8b0fdee0387d8c7dbdb2c7e2ef6afe119675492c
Raphael blehoue <raphaelsampan@gmail.com>
2016-11-10 18:22:41 +0000

    mise a jour du sidebar et ckeditor

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Labs\FrontBundle\LabsFrontBundle(),
             new Labs\BackBundle\LabsBackBundle(),
