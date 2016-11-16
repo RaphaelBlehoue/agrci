145219ca41328b9712a11f1eb541868c52e86016
Raphael blehoue <raphaelsampan@gmail.com>
2016-11-16 15:49:31 +0000

    mise en de la page d'accueil

--- src/Labs/FrontBundle/Controller/DefaultController.php
+++ src/Labs/FrontBundle/Controller/DefaultController.php
@@ -16,8 +16,35 @@ class DefaultController extends Controller
         $projects = $this->getProjectContent();
         return $this->render('LabsFrontBundle:Default:index.html.twig', [
             'about' => $about,
-            'projects' => $projects
+            'projects' => $projects,
         ]);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function SlideAction()
+    {
+        $slider = $this->getSlideContent();
+        return $this->render('LabsFrontBundle:includes:slide.html.twig', array(
+            'sliders' => $slider
+        ));
+    }
+
+    /**
+     * @Route("/notre_architect", name="architect")
+     */
+    public function ArchitectAction()
+    {
+        die('Notre architect');
+    }
+
+    /**
+     * @Route("/notre_project", name="project")
+     */
+    public function ProjectAction()
+    {
+        die('Notre project');
     }
 
     /**
@@ -39,5 +66,15 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $project = $em->getRepository('LabsBackBundle:Project')->findProjectLimit(8);
         return $project;
+    }
+
+    /**
+     * @return array|\Labs\BackBundle\Entity\Banner[]
+     */
+    private function getSlideContent()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $slides = $em->getRepository('LabsBackBundle:Banner')->findAll();
+        return $slides;
     }
 }
